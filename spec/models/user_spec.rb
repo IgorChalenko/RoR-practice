9c6a13b303abadd2494a46032ec29f3c6ce175cf
IgorChalenko <nobu598@gmail.com>
2021-05-10 22:52:59 +0300

    Done rubocop autocorrect

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,31 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject {
+  subject do
     described_class.new(username: 'igor',
                         email: 'igor@mail.com',
-                        password: '123456'
-    )
-  }
+                        password: '123456')
+  end
   describe 'Validations' do
-    it "is valid with valid attributes" do
+    it 'is valid with valid attributes' do
       expect(subject).to be_valid
     end
 
-    it "is fails without username attribute" do
+    it 'is fails without username attribute' do
       subject.username = nil
       expect(subject).to_not be_valid
     end
 
-    it "is fails without email attribute" do
+    it 'is fails without email attribute' do
       subject.email = nil
       expect(subject).to_not be_valid
     end
 
-    it "is fails without password attribute" do
+    it 'is fails without password attribute' do
       subject.password = nil
       expect(subject).to_not be_valid
     end
-  end  
+  end
 end
