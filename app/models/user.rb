eb5d90215e534619c459118de5260cfbefc88910
IgorChalenko <nobu598@gmail.com>
2021-05-18 23:01:07 +0300

    Add registration-page, form_partial, action create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
-
 # The User class
 class User < ApplicationRecord
+
   VALID_EMAIL_REGEX = /\A[\w+]+@[a-z\d]+\.[a-z]+\z/i
 
   has_secure_password
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@
 
 Rails.application.routes.draw do
   root 'pages#index'
-  resources :pages
+  get '/pages', to: 'pages#index'
+  get "/pages/registration", to: 'pages#new'
+  post "/pages/registration", to: 'pages#create'
 end
