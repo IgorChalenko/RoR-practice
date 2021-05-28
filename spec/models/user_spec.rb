8f18df64694be3faf22d5518f74f066cdfc67c62
IgorChalenko <nobu598@gmail.com>
2021-05-28 23:26:36 +0300

    delet coverage

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'Validations' do
-    let(:user) { build(:user) }
+    
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:email) }
     it { should have_secure_password }
